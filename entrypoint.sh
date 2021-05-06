73eb5f1ab7ceac8baaf43796efa27d4097bec237
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-06 17:21:19 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 apk update
-apk add xvfb fluxbox x11vnc build-base libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev mesa-dev mesa-dri-gallium xvfb-run xterm git openssh openssh-client python2 bash
+apk add xvfb fluxbox x11vnc build-base libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev mesa-dev mesa-dri-gallium xvfb-run xterm git nodejs npm python2 bash
 apk add virtualgl --update-cache --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
@@ -8,4 +8,5 @@ Xvfb -screen 0 900x720x24+32 +extension GLX &
 export DISPLAY=:0
 fluxbox &
 x11vnc -noshm -geometry 900x720 -shared -forever &
-ssh -R 80:localhost:80 nokey@localhost.run
+npm i localtunnel
+node node_modules/localtunnel/bin/lt.js --port 80 &
