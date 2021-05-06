33527dca5ca89cec3d74a2a8e174e8c87b34f6c9
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-06 18:13:34 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/ash
 apk update
-apk add xvfb fluxbox x11vnc build-base libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev mesa-dev mesa-dri-gallium xvfb-run xterm git python2 bash unzip wget
+apk add xvfb fluxbox x11vnc build-base libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev mesa-dev mesa-dri-gallium xvfb-run xterm bash unzip wget #git python2
 apk add virtualgl --update-cache --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted
-git clone https://github.com/ayunami2000/noVNC
-./noVNC/utils/launch.sh --listen 80 &
+#git clone https://github.com/ayunami2000/noVNC
+#./noVNC/utils/launch.sh --listen 80 &
 Xvfb -screen 0 900x720x24+32 +extension GLX &
 export DISPLAY=:0
 fluxbox &
@@ -11,7 +11,6 @@ x11vnc -noshm -geometry 900x720 -shared -forever &
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ./ngrok-stable-linux-amd64.zip
 chmod +x ./ngrok
-ls -a /
 ./ngrok authtoken $(cat /ngrok.txt)
 rm -rf /ngrok.txt
-./ngrok http 80
+./ngrok tcp 5900
