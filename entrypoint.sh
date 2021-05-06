9b8875ff6088397c090290679a36e8824c28b65b
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-06 17:31:05 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 apk update
-apk add xvfb fluxbox x11vnc build-base libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev mesa-dev mesa-dri-gallium xvfb-run xterm git nodejs npm python2 bash
+apk add xvfb fluxbox x11vnc build-base libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev mesa-dev mesa-dri-gallium xvfb-run xterm git python2 bash unzip#nodejs npm
 apk add virtualgl --update-cache --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
@@ -8,5 +8,9 @@ Xvfb -screen 0 900x720x24+32 +extension GLX &
 export DISPLAY=:0
 fluxbox &
 x11vnc -noshm -geometry 900x720 -shared -forever &
-npm i localtunnel
-node node_modules/localtunnel/bin/lt.js --port 80
+#npm i localtunnel
+#node node_modules/localtunnel/bin/lt.js --port 80
+wget https://github.com/angelobreuer/localtunnel-client/releases/download/1.0.1.0/linux-x64.zip
+unzip -e linux-x64.zip
+chmod +x Localtunnel
+./Localtunnel --port 80 http
